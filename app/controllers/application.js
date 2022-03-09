040329036b86810b1b73133a363495dd97451107
Jonathan Poelman <jonathan.poelman@protonmail.com>
2022-03-09 15:06:14 +0100

    added websocket client to applicationcontroller

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,7 +1,25 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
+import { io } from "socket.io-client";
 
 export default class ApplicationController extends Controller {
   @service session;
   @service account;
+
+  constructor(){
+    super(...arguments);
+
+    const socket = io('http://localhost:3002');
+
+    // const socket = this.socketIOService.socketFor('http://localhost:3002');
+
+
+    socket.on('new post', () => {
+      console.log('New post!')
+    });
+  }
+
+  newPostHandler(event){
+    console.log(event.data);
+  }
 }
